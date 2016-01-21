929e9eae461cef6197b4388439f411ff57c414c9
Stiven <stivenbr12@gmail.com>
2016-01-21 01:21:42 -0500

    Diseño cargos php, verificar porque no funciona el $.post

--- application/views/administrativo/cargos.php
+++ application/views/administrativo/cargos.php
@@ -27,7 +27,7 @@
                                     <label for="cargojefe" class="control-label col-md-3">Cargo jefe directo</label>
                                     <div class="col-md-9">
                                         <select name="cargojefe" id="cargojefe" class="form-control select2me" >
-                                            <option value="">::Seleccionar::</option>
+                                            <option value=""></option>
                                             <?php foreach ($cargo as $d) { ?>
                                                 <option value="<?php echo $d->car_id ?>"><?php echo $d->car_nombre ?></option>
                                             <?php } ?>
@@ -73,25 +73,39 @@
                     <table class="table table-striped table-bordered table-hover" id="sample_2">
                         <thead>
                             <tr>
-                                <th>Prueba1</th>
-                                <th>Prueba2</th>
-                                <th>Prueba3</th>
-                                <th>Prueba4</th>
+                                <th>Cargo</th>
+                                <th>Cargo Jefe Directo</th>
+                                <th>% Cotización ARL</th>
+                                <th>Riesgos</th>
+                                <th>Editar</th>
+                                <th>Eliminar</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>2</td>
-                                <td>2</td>
-                                <td>2</td>
-                                <td>2</td>
-                            </tr>
-                            <tr>
-                                <td>1</td>
-                                <td>1</td>
-                                <td>1</td>
-                                <td>1</td>
-                            </tr>
+                            <?php foreach ($cargo as $c) { ?>
+                                <tr>
+                                    <td><?php echo $c->car_nombre ?></td> 
+                                    <td><?php echo $c->jefe ?></td> 
+                                    <td style="text-align:center;"><?php echo $c->car_porcentajearl ?></td> 
+                                    <td style="text-align: center">
+                                        <?php if ($c->cantidadRiesgos > 0): ?>
+                                            <i class="fa fa-child fa-2x riesgo " title="Eliminar" car_id="<?php echo $c->car_id ?>" ></i>
+                                        <?php endif; ?>
+                                    </td>
+                                    <td>
+                                        <a href="javascript:;" class="btn btn-xs default modificar" car_id="<?php echo $c->car_id ?>">
+                                            <i class="fa fa-pencil-square-o" title="Modificar" ></i>
+                                            Modificar
+                                        </a>
+                                    </td> 
+                                    <td>
+                                        <a href="javascript:;" class="btn btn-xs default eliminar" car_id="<?php echo $c->car_id ?>">
+                                            <i class="fa fa-trash-o" title="Eliminar" car_id="<?php echo $c->car_id ?>"></i>
+                                            Eliminar
+                                        </a>
+                                    </td> 
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
@@ -100,80 +114,6 @@
     </div>
 </div>
 
-
-<div class="row">
-    <div class="circuloIcon guardarcargo" tittle="Guardar"><i class="fa fa-floppy-o fa-3x"></i></div>
-</div>
-<div class="row">
-    <div class="col-md-12">
-        <div class="tituloCuerpo">
-            <span class="txtTitulo">
-                <a href="<?php echo base_url("index.php/presentacion/principal") ?>">HOME</a>/
-                <a href="<?php echo base_url("index.php/administrativo/empresa") ?>">EMPRESA</a>/
-                CARGOS</span>
-        </div>
-    </div>
-</div>
-<div class='cuerpoContenido'>
-    <div class="row">
-        <form method="post" id="formcargos" class="form-horizontal">
-            <div class="form-group">
-                <label for="id" class="col-md-1 control-label"><span class="campoobligatorio">*</span>Cargo</label>
-                <div class="col-md-3">
-                    <input type="text" class="form-control obligatorio texto" name="cargo" id="cargo" />
-                </div>
-                <label for="id" class="col-md-1 control-label">Cargo jefe directo</label>
-                <div class="col-md-3">
-                    <select name="cargojefe" id="cargojefe" class="form-control texto" >
-                        <option value="">::Seleccionar::</option>
-                        <?php foreach ($cargo as $d) { ?>
-                            <option value="<?php echo $d->car_id ?>"><?php echo $d->car_nombre ?></option>
-                        <?php } ?>
-                    </select>
-                </div>
-                <label for="id" class="col-md-1 control-label"><span class="campoobligatorio">*</span>%Cotizacion ARL</label>
-                <div class="col-md-3">
-                    <input type="text" name="porcentaje" id="porcentaje" class="form-control obligatorio number2 texto" />
-                </div>
-            </div>
-        </form>
-    </div>
-    <div class="row">
-        <div class="col-md-12">
-            <table class="tablesst">
-                <thead>
-                <th>Cargo</th>
-                <th>Cargo Jefe Directo</th>
-                <th>% Cotización ARL</th>
-                <th>Riesgos</th>
-                <th>Editar</th>
-                <th>Eliminar</th>
-                </thead>
-                <tbody id="bodycargo">
-                    <?php foreach ($cargo as $c) { ?>
-                        <tr>
-                            <td><?php echo $c->car_nombre ?></td> 
-                            <td><?php echo $c->jefe ?></td> 
-                            <td style="text-align:center;"><?php echo $c->car_porcentajearl ?></td> 
-                            <td style="text-align: center" class="transparent">
-                                <?php if ($c->cantidadRiesgos > 0): ?>
-                                    <i class="fa fa-child fa-2x riesgo " title="Eliminar" car_id="<?php echo $c->car_id ?>" ></i>
-                                <?php endif; ?>
-                            </td>
-                            <td class="transparent">
-                                <i class="fa fa-pencil-square-o fa-2x modificar" title="Modificar" car_id="<?php echo $c->car_id ?>"  ></i>
-                            </td> 
-                            <td class="transparent">
-                                <i class="fa fa-trash-o fa-2x eliminar" title="Eliminar" car_id="<?php echo $c->car_id ?>"></i>
-                            </td> 
-                        </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
-        </div>
-    </div>
-
-</div>
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
     <div class="modal-dialog" role="document">
