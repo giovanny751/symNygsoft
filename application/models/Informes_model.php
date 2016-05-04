14731e2bda5dad31e3a14c615d9bf677f7aa1140
Gerson Javier <gerson.barbosa@mgms.com.co>
2016-05-04 18:05:41 -0500

    informe
    informe

--- application/models/Informes_model.php
+++ application/models/Informes_model.php
@@ -5,22 +5,27 @@ class Informes_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
-    function horasExtras($empleado,$fechaDesde,$fechaHasta){
+
+    function horasExtras($empleado, $fechaDesde, $fechaHasta) {
         $this->db->order_by('empHorExt_fecha');
-        if(!empty($empleado))$this->db->where("empleado.emp_id ",$empleado);
-        if(!empty($fechaDesde))$this->db->where("empleado_horas_extra.empHorExt_fecha >=",$fechaDesde);
-        if(!empty($fechaHasta))$this->db->where("empleado_horas_extra.empHorExt_fecha <=",$fechaHasta);
+        if (!empty($empleado))
+            $this->db->where("empleado.emp_id ", $empleado);
+        if (!empty($fechaDesde))
+            $this->db->where("empleado_horas_extra.empHorExt_fecha >=", $fechaDesde);
+        if (!empty($fechaHasta))
+            $this->db->where("empleado_horas_extra.empHorExt_fecha <=", $fechaHasta);
         $this->db->select("empleado.Emp_Nombre");
         $this->db->select("empleado.Emp_Apellidos");
         $this->db->select("empleado_horas_extra.empHorExt_horas");
         $this->db->select("empleado_horas_extra.empHorExt_fecha");
         $this->db->select("hora_extra_tipo.horExtTip_tipo");
-        $this->db->join("hora_extra_tipo","hora_extra_tipo.horExtTip_id = empleado_horas_extra.horExtTip_id");
-        $this->db->join("empleado","empleado.Emp_Id = empleado_horas_extra.emp_id");
+        $this->db->join("hora_extra_tipo", "hora_extra_tipo.horExtTip_id = empleado_horas_extra.horExtTip_id");
+        $this->db->join("empleado", "empleado.Emp_Id = empleado_horas_extra.emp_id");
         $horas = $this->db->get("empleado_horas_extra");
         return $horas->result();
     }
-    function informeGeneral(){
+
+    function informeGeneral() {
         $query = "
             select 
                 (select count(pla_id) from planes where est_id = 1) as planes,
@@ -46,25 +51,43 @@ class Informes_model extends CI_Model {
                 (select count(bot_id) from botiquin where est_id = 3 ) as botiquinEliminados
                     ";
         return $this->db->query($query)->result();
-        
     }
-    
-    function informeEmpleado(){
+
+    function informeEmpleado() {
         $query = "select 
 	sum(if(Est_id = 1,1,0)) as empleadosActivos, 
-        sum(if(Est_id = 2,1,0)) as empleadosInactivos,
+        (
+        select sum(if(est_id = 1,1,0)) from empleado 
+        join empleado_contratos on empleado_contratos.emp_id = empleado.Emp_id
+        where 
+        est_id = 1 and 
+        ( empleado_contratos.empCon_fechaHasta <= '" . date("Y-m-d") . "' and empleado_contratos.empCon_fechaHasta != '0000-00-00 00:00:00')
+        ) as empleadosInactivos,
         sum(if(Est_id = 3,1,0)) as empleadosEliminados,
               sum(if(sex_Id = 1 and Est_id = 1,1,0)) as empleadosHombres, 
-              sum(if(sex_Id = 1 and Est_id = 2,1,0)) as empleadosHombresInactivos,
+              (
+        select sum(if(sex_id = 1,1,0)) from empleado 
+        join empleado_contratos on empleado_contratos.emp_id = empleado.Emp_id
+        where 
+        est_id = 1 and 
+        ( empleado_contratos.empCon_fechaHasta <= '" . date("Y-m-d") . "' and empleado_contratos.empCon_fechaHasta != '0000-00-00 00:00:00')
+        ) as empleadosHombresInactivos,
               sum(if(sex_Id = 1 and Est_id = 3,1,0)) as empleadosHombresEliminados,
               sum(if(sex_Id = 2 and Est_id = 1,1,0)) as empleadosMujeres,
-              sum(if(sex_Id = 2 and Est_id = 2,1,0)) as empleadosMujeresInactivos,
+              (
+        select sum(if(sex_id = 2,1,0)) from empleado 
+        join empleado_contratos on empleado_contratos.emp_id = empleado.Emp_id
+        where 
+        est_id = 1 and 
+        ( empleado_contratos.empCon_fechaHasta <= '" . date("Y-m-d") . "' and empleado_contratos.empCon_fechaHasta != '0000-00-00 00:00:00')
+        ) as empleadosMujeresInactivos,
               sum(if(sex_Id = 2 and Est_id = 3,1,0)) as empleadosMujeresEliminados
       from empleado;";
-        
+
         return $this->db->query($query)->result();
     }
-    function informePqr(){
+
+    function informePqr() {
         $query = "select 
                         sum(if(tipSol_id = 1 and est_id = 1,1,0)) as queja,
                         sum(if(tipSol_id = 2 and est_id = 1,1,0)) as peticion, 
@@ -82,5 +105,5 @@ class Informes_model extends CI_Model {
                         pqr";
         return $this->db->query($query)->result();
     }
-    
+
 }
